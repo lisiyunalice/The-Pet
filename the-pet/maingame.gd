cee3fa69f52cbdea2efe18a78dd4cb8650bfd366
JunxinJ <jjiang15@sva.edu>
2025-10-20 17:31:30 -0400

    Revert "main scene"
    This reverts commit 6c80a32155bfe84b4b91d99e739684af1bef2a2f.

--- the-pet/maingame.gd
+++ the-pet/maingame.gd
@@ -4,7 +4,3 @@ extends Node2D
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://minigames/hunger/HungerMiniGame.tscn")
 	pass # Replace with function body.
-
-func _on_button_2_pressed() -> void:
-	get_tree().change_scene_to_file("res://minigames/Happiness/Gourmet Feeding.tscn")
-	pass # Replace with function body.
